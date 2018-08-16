23dee291e465a1ed083a869d9aabc37b7e7a4508
Welkson Ramos <welksonr@hotmail.com>
2018-08-16 14:17:55 -0300

    Update test :pencil:

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import execa from 'execa';
 import pkg from './package.json';
 
 test('Deve ser retornada o nome e a versão correta do módulo quando o parâmetro --version for usado', async t => {
-  const {stdout} = await execa('./lib/awesome-br.js', ['--version']);
+  const { stdout } = await execa('./lib/awesome-br.js', ['--version']);
+  console.log(stdout);
   t.is(stdout, `${pkg.name}: ${pkg.version}`);
 });
